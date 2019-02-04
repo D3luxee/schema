b3d8ca91398b0db2271f300b31beab93b820faef
D3luxee <georg@neuland.tech>
2019-02-04 08:49:36 +0100

    Add more support functions

--- schema.go
+++ schema.go
@@ -1,6 +1,8 @@
 package schema
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"strings"
 
 	"github.com/raintank/schema"
@@ -10,7 +12,7 @@ type MetricData struct {
 	*schema.MetricData
 }
 
-func (m *MetricData) GetTags() map[string]string {
+func (m *MetricData) GetTagMap() map[string]string {
 	tags := make(map[string]string)
 	for _, v := range m.Tags {
 		kv := strings.Split(v, "=")
@@ -19,4 +21,28 @@ func (m *MetricData) GetTags() map[string]string {
 		}
 	}
 	return tags
+}
+
+func (m *MetricData) GetTag(key string) string {
+	for _, v := range m.Tags {
+		if strings.HasPrefix(v, key) {
+			kv := strings.Split(v, "=")
+			if len(kv) == 2 {
+				return kv[1]
+			}
+		}
+
+	}
+	return ""
+}
+
+func (m *MetricData) AddTag(key, value string) {
+	m.Tags = append(m.Tags, key+"="+value)
+}
+
+func (m *MetricData) LookupID() string {
+	h := sha256.New()
+	h.Write([]byte(m.GetTag("hostname")))
+	h.Write([]byte(m.Name))
+	return hex.EncodeToString(h.Sum(nil))
 }
